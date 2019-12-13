1a7d3358ea981c221f28d7076f74c0677821c956
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-13 12:54:38 +0300

    (chore): add globalSetup to clear db for mongo example

---
+++ examples/mongo/test/globalSetup.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { connect, disconnect } = require('../app/config/database');
+
+module.exports = async () => {
+  await connect();
+  await mongoose.connection.dropDatabase();
+  await disconnect();
+};
